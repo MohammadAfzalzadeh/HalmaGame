5abf0d0d726c4ee525a84f4c86d9aa6d35bb718f
mohammad_afzalzadeh <mohammad.afzalzadeh.3@gmail.com>
2021-01-11 12:48:26 +0330

    add function PosInScreanToArry & ArryToPosInScrean & is_x_y_in_table into basick function

--- game_v1/basickFunction.h
+++ game_v1/basickFunction.h
@@ -24,3 +24,16 @@ void setTextColor(int textColor, int backColor) {
 	int colorAttribute = backColor << 4 | textColor;
 	SetConsoleTextAttribute(consoleHandle, colorAttribute);
 }
+void PosInScreanToArry(int pos_x, int pos_y, int& x, int& y) {
+	x = (pos_x + 1) / 2 -1;
+	y = pos_y / 2;
+}
+void ArryToPosInScrean(int &pos_x, int &pos_y, int x, int y) {
+	pos_x = (x + 1) * 2 - 1;
+	pos_y = 2 * y+1;
+}
+int is_x_y_in_table(int x,int y,int n) {
+	if (x<0 || x>n - 1 || y<0 || y>n - 1)
+		return 0;
+	return 1;
+}
