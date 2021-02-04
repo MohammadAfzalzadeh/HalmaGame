0e5f5ef4e77c990d107b158393783dc650fde4b4
mohammad_afzalzadeh <mohammad.afzalzadeh.3@gmail.com>
2021-02-04 18:22:13 +0330

    add option to go to amar page

--- game_v1/nahveh_anjam_bazi.h
+++ game_v1/nahveh_anjam_bazi.h
@@ -1,28 +1,37 @@
 #pragma once
 
-int nahveh_bazi() {
+int nahveh_bazi(char name[]) {
+	while (1)
+	{
 		setTextColor(font_color, back_color);
 		system("cls");
-		int answer,menu_count;
+		int answer, menu_count;
 		if (IsExistSaveFile()) {
 			printf("yek mored ra entekhab konid\n");
 			printf("1)bazi gadid\n");
 			printf("2)bazi ba save shodeh\n");
-			printf("3)EXIT");
-			menu_count = 3;
+			printf("3)show my statistics and best statistics and worst statistics\n");
+			printf("4)EXIT");
+			menu_count = 4;
 			answer = move_with_keyboard_menu(menu_count);
 		}
 		else
 		{
 			printf("yek mored ra entekhab konid\n");
 			printf("1)bazi gadid\n");
-			printf("2)EXIT");
-			menu_count = 2;
+			printf("2)show my statistics and best statistics and worst statistics\n");
+			printf("3)EXIT");
+			menu_count = 3;
 			answer = move_with_keyboard_menu(menu_count);
 		}
 		if (answer == menu_count) {
 			gotoxy2(0, menu_count + 1);
 			exit(0);
 		}
-		return answer;
+		if (answer == menu_count - 1) {
+			print_worst_and_best_player(name);
+		}
+		else
+			return answer;
+	}
 }
